f6ac1e2df2eb87ac1bdb713bdceb2e425ca847b1
AleSchmidt33 <aleschmoschi@gmail.com>
2026-05-05 22:00:38 -0300

    Conexion mongo exitosa - modelo de usuario creado

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,14 +1,10 @@
-/// CORAZON DE LA APP
-
-require('dotenv').config();
+require('dotenv').config({ path: './config/.env' });
 const express = require('express');
+const mongoConnect = require('./config/mongoConnet'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-
-
+mongoConnect();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
