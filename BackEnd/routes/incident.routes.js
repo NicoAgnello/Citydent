bbf7bdcd05820254e6991a4464ae642d9c3fdcb6
NicoAgnello <nikoagnello1@gmail.com>
2026-05-25 19:59:03 -0300

    fix: integrar aiIncidentValidation en el POST de incidentes

--- BackEnd/routes/incident.routes.js
+++ BackEnd/routes/incident.routes.js
@@ -6,15 +6,9 @@ const { aiIncidentValidation } = require('../middlewares/aiIncidentValidation');
 const middleClaudinary = require('../middlewares/claudinary')
 const { create, getMyIncidents, getAll, updateStatus, updateCategory } = require('../controllers/incident.controller');
 
-router.post('/', authMiddleware, verifyRole('user', 'admin', 'superAdmin'),middleClaudinary, create);
+router.post('/', authMiddleware, verifyRole('user', 'admin', 'superAdmin'), middleClaudinary, aiIncidentValidation, create);
 router.get('/my-incidents', authMiddleware, verifyRole('user', 'admin', 'superAdmin'), getMyIncidents);
 router.get('/', authMiddleware, verifyRole('admin', 'superAdmin'), getAll);
 router.patch('/:id/status', authMiddleware, verifyRole('admin', 'superAdmin'), updateStatus);
 router.patch('/:id/category', authMiddleware, verifyRole('admin', 'superAdmin'), updateCategory);
-router.post(
-  '/', 
-  verifyToken, 
-  aiIncidentValidation, 
-  incidentController.create
-);
 module.exports = router;
